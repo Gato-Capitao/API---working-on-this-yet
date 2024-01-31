f138a8d6c9602e6604d32f89c0cbc7fe54408413
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-01-31 18:03:45 +0000

    Ajuste no servidor

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { loomiRouter } from "./src/routes/Loomian.route.js";
+import { BookRouter } from "./src/routes/Book.route.js";
+import { MovieRouter } from "./src/routes/Movie.route.js";
+import { GameRouter } from "./src/routes/Game.route.js";
 const app = express();
-const port = 8002;
+const port = 8009;
 
 app.use(express.json());
-app.use(loomiRouter);
+app.use(BookRouter);
+app.use(MovieRouter);
+app.use(GameRouter);
 
 app.listen(port, ()=> {
   console.log("started server");
