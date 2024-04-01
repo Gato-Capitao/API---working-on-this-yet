592831213e9e690c5b3f7491a7d7dfbcee884164
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-04-01 22:08:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from "express";
 import { BookRouter } from "./src/routes/Book.route.js";
 import { MovieRouter } from "./src/routes/Movie.route.js";
 import { GameRouter } from "./src/routes/Game.route.js";
+import cors from 'cors'
+
+
 const app = express();
 const port = 8009;
 
 app.use(express.json());
+app.use(cors())
 app.use(BookRouter);
 app.use(MovieRouter);
 app.use(GameRouter);
